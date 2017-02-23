bdc4352ffe9dcf5030a75ba90cdd1f83bdfc294e
David Marcos! <davidmarcosg98@gmail.com>
2017-02-23 19:44:26 +0100

    Modified Video.java

--- src/Video.java
+++ src/Video.java
@@ -7,13 +7,35 @@ public class Video {
     private Server data_center = new Server();
     private Server[] cache;
 
-    Video(double size, Server data_center, int caches){
-        cache = new Server[caches];
+    Video(double size, Server data_center, double caches){
+
+        setSize(size);
+        setDataCenter(data_center);
+        setCacheServers(caches);
+
+    }
+
+    public void setSize(double size){
+        this.size = size;
+    }
+
+    public void setDataCenter(Server data_center){
+        this.data_center = data_center;
+    }
+
+    public void setCacheServers(double caches){
+        this.cache = new Server[caches];
         for(int ii = 0; ii < caches; ii++){
-            cache[ii] = new Server();
-            this.size = size;
-            
+            this.cache[ii] = new Server();
         }
+    }
+
+    public double getSize(){
+        return this.size;
+    }
+
+    public Server[] getCache(){
+        return this.cache;
     }
 
 }
